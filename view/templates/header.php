c1c44ac68fa579edc54ef09b9b76d0de76f90255
Tim Waterham <99021474@mydavinci.nl>
2017-05-16 13:43:45 +0200

    Add files via upload

---
+++ view/templates/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Kalender</title>	
+	<link rel="stylesheet" href="<?= URL ?>/css/main.css">
+</head>
+<body>
+	<nav>
+		<ul>
+			<li><a href="<?= URL ?>index">Home</a></li>
+		</ul>
+	</nav>
